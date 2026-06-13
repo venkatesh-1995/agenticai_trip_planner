d8ffb0f3a72bb71abde1ce20593182e706f0e89a
venkat_ramana <ovenkatramana01@gmail.com>
2026-06-13 10:08:26 +0000

    updated

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ print(get_requirements())
 setup(
     name="AI-TRAVEL-PLANNER",
     version="0.0.1",
-    author="sunny savita",
-    author_email="snshrivas3365@gmail.com",
+    author="venkat-ramana",
+    author_email="ovenkatramana01@gmail.com",
     packages = find_packages(),
     install_requires=get_requirements()
 )
